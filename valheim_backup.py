6c9ef30288ab1469edfeb15f3b0b60ee00ed6be8
Sven Hergenhahn <sven.hergenhahn@dm.de>
2021-03-23 18:07:21 +0100

    refactored error handling, reduces # of loops to 1

--- valheim_backup.py
+++ valheim_backup.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from glob import glob
 import tarfile
@@ -6,43 +7,53 @@ from datetime import datetime
 from pytz import timezone
 import inotify.adapters
 from twisted.internet.task import LoopingCall
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
+from twisted.python import log
 
 #VALHEIM_PATH = Path('~/.config/valheim')
 VALHEIM_PATH = Path('./testdir')
 VALHEIM_WORLDS = VALHEIM_PATH / 'worlds'
 VALHEIM_CHARS = VALHEIM_PATH / 'characters'
 BACKUP_PATH = VALHEIM_PATH / 'backup'
+SUFFIXES = ['db', 'fwl', 'fff']
+LOOP_TIME = 300 # seconds
 
+#EVENT='IN_WRITE_CLOSE'
 EVENT='IN_MODIFY'
 
 def _main():
-    lc = LoopingCall(backup_world)
-    lc.start(600)
+    #log.startLogging(sys.stdout)
+    observer = log.PythonLoggingObserver()
+    observer.start()
 
-    lc2 = LoopingCall(backup_character)
-    lc2.start(300)
+    lc = LoopingCall(backup_loop)
+    d = lc.start(LOOP_TIME)
+    d.addErrback(errback)
 
     reactor.run()
 
-def backup_world():
-    WORLDFILE = detect_state(VALHEIM_WORLDS, '.db')
-    backup('world', WORLDFILE, BACKUP_PATH)
+def errback(message):
+    log.msg('Nothing found %s' % message)
 
-def backup_character():
-    CHARFILE = detect_state(VALHEIM_CHARS, '.fwl')
-    backup('character', CHARFILE, BACKUP_PATH)
+def backup_loop():
+    modified = detect_state([VALHEIM_WORLDS, VALHEIM_CHARS], SUFFIXES)
+    if modified:
+        backup(modified, BACKUP_PATH)
 
-def detect_state(path, suffix):
+def detect_state(paths, suffixes=SUFFIXES):
     i = inotify.adapters.Inotify()
-    i.add_watch(str(path))
-    for event in i.event_gen(yield_nones=False, timeout_s=280):
+    for path in paths:
+        i.add_watch(str(path))
+    for event in i.event_gen(yield_nones=False, timeout_s=LOOP_TIME - 10):
         (_, type_names, path, filename) = event
-        if EVENT in type_names and filename.endswith(suffix):
+        if EVENT in type_names and filename.split('.')[-1] in suffixes:
+            logger.info('Found event %s and will start backup of %s' % (','.join(type_names), filename))
             return Path(path) / filename
 
-def backup(kind, filepath, backup_path):
+def backup(filepath, backup_path):
+    log.msg('Starting backup for %s' % filepath)
     item = filepath.stem
+    kind = filepath.parent.stem
     now = datetime.now().astimezone().strftime("%Y-%m-%d_%H:%M:%S")
     zipdest = f'{backup_path}/{item}-{kind}-{now}.tgz'
     with tarfile.open(zipdest, "w:gz") as tar:
