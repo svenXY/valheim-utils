b345ae7bbcc8c08c96064ea5658ee86e5f8ddbc2
Sven Hergenhahn <sven.hergenhahn@dm.de>
2021-04-18 23:19:43 +0200

    working version with IM_MOVED_TO

--- valheim_backup.py
+++ valheim_backup.py
@@ -10,16 +10,16 @@ from twisted.internet.task import LoopingCall
 from twisted.internet import reactor, defer
 from twisted.python import log
 
-#VALHEIM_PATH = Path('~/.config/valheim')
-VALHEIM_PATH = Path('./testdir')
+VALHEIM_PATH = Path('/home/svh/.config/unity3d/IronGate/Valheim')
+#VALHEIM_PATH = Path('./testdir')
 VALHEIM_WORLDS = VALHEIM_PATH / 'worlds'
 VALHEIM_CHARS = VALHEIM_PATH / 'characters'
-BACKUP_PATH = VALHEIM_PATH / 'backup'
-SUFFIXES = ['db', 'fwl', 'fff']
+BACKUP_PATH = '/media/work/valheim'
+SUFFIXES = ['db', 'fch']
 LOOP_TIME = 300 # seconds
 
-#EVENT='IN_WRITE_CLOSE'
-EVENT='IN_MODIFY'
+EVENT='IN_MOVED_TO'
+NONEEVENT= ['IN_ACCESS']
 
 def _main():
     #log.startLogging(sys.stdout)
@@ -34,34 +34,48 @@ def _main():
 
 def errback(message):
     log.msg('Nothing found %s' % message)
+    logger.warning('Nothing found %s' % message)
 
 def backup_loop():
-    modified = detect_state([VALHEIM_WORLDS, VALHEIM_CHARS], SUFFIXES)
+    logger.info('Entering loop...')
+    modified = detect_save_cycle([VALHEIM_WORLDS, VALHEIM_CHARS], SUFFIXES)
     if modified:
         backup(modified, BACKUP_PATH)
 
-def detect_state(paths, suffixes=SUFFIXES):
+def detect_save_cycle(paths, suffixes=SUFFIXES):
+    logger.info('Entering detect...')
+    savefiles = []
     i = inotify.adapters.Inotify()
     for path in paths:
         i.add_watch(str(path))
     for event in i.event_gen(yield_nones=False, timeout_s=LOOP_TIME - 10):
         (_, type_names, path, filename) = event
-        if EVENT in type_names and filename.split('.')[-1] in suffixes:
-            logger.info('Found event %s and will start backup of %s' % (','.join(type_names), filename))
-            return Path(path) / filename
+        suffix = filename.split('.')[-1]
+        if EVENT not in NONEEVENT:
+            logger.info('Event detected: %s on %s', ','.join(type_names), filename)
+            logger.info("Suffix: %s", filename.split('.')[-1])
+        if EVENT in type_names and suffix in suffixes:
+            logger.info('Found event %s and will consider %s' % (','.join(type_names), filename))
+            savefiles.append(Path(path) / filename)
+            if suffix == 'db':
+                return savefiles
+    logger.info('timeout to find active files')
+    return None
 
-def backup(filepath, backup_path):
-    log.msg('Starting backup for %s' % filepath)
-    item = filepath.stem
-    kind = filepath.parent.stem
-    now = datetime.now().astimezone().strftime("%Y-%m-%d_%H:%M:%S")
-    zipdest = f'{backup_path}/{item}-{kind}-{now}.tgz'
-    with tarfile.open(zipdest, "w:gz") as tar:
-        for i in glob(f'{filepath.parent}/{item}.*'):
-            tar.add(i, arcname=Path(i).name)
+def backup(files, backup_path):
+    for filepath in files:
+        log.msg('Starting backup for %s' % filepath)
+        logger.info('Starting backup for %s' % filepath)
+        item = filepath.stem
+        kind = filepath.parent.stem
+        now = datetime.now().astimezone().strftime("%Y-%m-%d_%H:%M:%S")
+        zipdest = f'{backup_path}/{item}-{kind}-{now}.tgz'
+        with tarfile.open(zipdest, "w:gz") as tar:
+            for i in glob(f'{filepath.parent}/{item}.*'):
+                tar.add(i, arcname=Path(i).name)
 
 def start_logging():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     logging.Formatter.converter = lambda *args: datetime.now(tz=timezone('Europe/Berlin')).timetuple()
     return logging.getLogger('_main_')
 
--- valheim_inotify.py
+++ valheim_inotify.py
@@ -5,29 +5,32 @@ from pytz import timezone
 from datetime import datetime
 
 
-#VALHEIM_PATH = Path('~/.config/valheim')
-VALHEIM_PATH = Path('./testdir')
+VALHEIM_PATH = Path('/home/svh/.config/unity3d/IronGate/Valheim')
+#VALHEIM_PATH = Path('./testdir')
 VALHEIM_WORLDS = VALHEIM_PATH / 'worlds'
 VALHEIM_CHARS = VALHEIM_PATH / 'characters'
 BACKUP_PATH = VALHEIM_PATH / 'backup'
 
-EVENT='IN_MODIFY'
+EVENT='IN_MOVED_TO'
 
 def _main():
-    WORLDFILE = detect_state(VALHEIM_WORLDS, '.db')
+    WORLDFILE = detect_state([VALHEIM_WORLDS, VALHEIM_CHARS], '.db')
 
-def detect_state(path, suffix):
+def detect_state(paths, suffix):
     i = inotify.adapters.Inotify()
-    i.add_watch(str(path))
+    for p in paths:
+        i.add_watch(str(p))
 
-    for event in i.event_gen(yield_nones=False, timeout_s=60):
+    for event in i.event_gen(yield_nones=False):
         (_, type_names, path, filename) = event
 
         logger.debug("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
               path, filename, type_names))
+        if EVENT in type_names:
+            logger.info("%s: %s", filename, type_names)
 
 def start_logging():
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(message)s' )
+    logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s' )
     logging.Formatter.converter = lambda *args: datetime.now(tz=timezone('Europe/Berlin')).timetuple()
     return logging.getLogger('_main_')
 
