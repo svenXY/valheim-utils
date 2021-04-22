9a1c158c96fed9d30bd0ca8c72e707f5cb9420c8
Sven Hergenhahn <sven.hergenhahn@dm.de>
2021-04-22 17:26:51 +0200

    Better loglevels

--- valheim_backup.py
+++ valheim_backup.py
@@ -20,11 +20,13 @@ LOOP_TIME = 300 # seconds
 
 EVENT='IN_MOVED_TO'
 NONEEVENT= ['IN_ACCESS']
+FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 
 def _main():
-    #log.startLogging(sys.stdout)
     observer = log.PythonLoggingObserver()
     observer.start()
+
+    logger.info('Background backup process started in %s', VALHEIM_PATH)
 
     lc = LoopingCall(backup_loop)
     d = lc.start(LOOP_TIME)
@@ -33,17 +35,16 @@ def _main():
     reactor.run()
 
 def errback(message):
-    log.msg('Nothing found %s' % message)
     logger.warning('Nothing found %s' % message)
 
 def backup_loop():
-    logger.info('Entering loop...')
+    logger.debug('Entering loop...')
     modified = detect_save_cycle([VALHEIM_WORLDS, VALHEIM_CHARS], SUFFIXES)
     if modified:
         backup(modified, BACKUP_PATH)
 
 def detect_save_cycle(paths, suffixes=SUFFIXES):
-    logger.info('Entering detect...')
+    logger.debug('Entering detect...')
     savefiles = []
     i = inotify.adapters.Inotify()
     for path in paths:
@@ -52,30 +53,32 @@ def detect_save_cycle(paths, suffixes=SUFFIXES):
         (_, type_names, path, filename) = event
         suffix = filename.split('.')[-1]
         if EVENT not in NONEEVENT:
-            logger.info('Event detected: %s on %s', ','.join(type_names), filename)
-            logger.info("Suffix: %s", filename.split('.')[-1])
+            logger.debug('Event detected: %s on %s', ','.join(type_names), filename)
         if EVENT in type_names and suffix in suffixes:
             logger.info('Found event %s and will consider %s' % (','.join(type_names), filename))
             savefiles.append(Path(path) / filename)
             if suffix == 'db':
+                logger.info('World has ben updated, starting backup for all detected modified files')
                 return savefiles
-    logger.info('timeout to find active files')
+    logger.info('End of cycle reached without detecting relevant file changes, waiting for next cycle to start')
     return None
 
 def backup(files, backup_path):
     for filepath in files:
-        log.msg('Starting backup for %s' % filepath)
-        logger.info('Starting backup for %s' % filepath)
-        item = filepath.stem
-        kind = filepath.parent.stem
-        now = datetime.now().astimezone().strftime("%Y-%m-%d_%H:%M:%S")
-        zipdest = f'{backup_path}/{item}-{kind}-{now}.tgz'
-        with tarfile.open(zipdest, "w:gz") as tar:
-            for i in glob(f'{filepath.parent}/{item}.*'):
-                tar.add(i, arcname=Path(i).name)
+        try:
+            item = filepath.stem
+            kind = filepath.parent.stem
+            now = datetime.now().astimezone().strftime("%Y-%m-%d_%H:%M:%S")
+            zipdest = f'{backup_path}/{item}-{kind}-{now}.tgz'
+            with tarfile.open(zipdest, "w:gz") as tar:
+                for i in glob(f'{filepath.parent}/{item}.*'):
+                    tar.add(i, arcname=Path(i).name)
+            logger.info('Backup written to %s' % zipdest)
+        except Exception:
+            logger.exception('Failed to write backup to %s', zipdest)
 
 def start_logging():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
     logging.Formatter.converter = lambda *args: datetime.now(tz=timezone('Europe/Berlin')).timetuple()
     return logging.getLogger('_main_')
 
